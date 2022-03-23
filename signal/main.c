4896c6c46f8061a341724eb476901b0f143c5063
c0rrr <eryk.surel@onet.pl>
2022-03-23 08:28:07 +0000

    Minor signal fixes

--- signal/main.c
+++ signal/main.c
@@ -4,14 +4,23 @@
 #include <unistd.h>
 #include <string.h>
 
+volatile int Flag=1;
+
 void handler(int signum){
-	printf("signal!\n");
-	printf("%s\n", strsignal(signum));
+	Flag = 0;
+	printf("Controlled exit");
+	//printf("signal!\n");
+	//printf("%s\n", strsignal(signum));
+}
+
+void handler2(int signum){
+	printf("Interrupt");
 }
 
 int main(void){
-	signal(SIGINT, handler);
-	while(1)
+	signal(SIGQUIT, handler);
+	signal(SIGINT, handler2);
+	while(Flag)
 	{
 		printf("working...\n");
 		sleep(1);
