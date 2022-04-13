2b7160001e79b3268bc467c1a324dc214dcf8634
c0rrr <eryk.surel@onet.pl>
2022-04-13 07:10:04 +0000

    Copy-projekt

--- projekt/copy.c
+++ projekt/copy.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void Err(char* errc)
+{
+	printf("1");
+	perror(errc);
+	return;
+}
 
 void CpFile(char* src, char* dest)
 {
+	char buf[128];
+	int fileread, inft;
+	int outft = open(dest, O_CREAT| O_APPEND| O_RDWR);
+	if(outft == -1) Err("open");
+	inft = open(src, O_RDONLY);
+	if(inft >0)
+	{
+		while((fileread = read(inft, buf, sizeof(buf))) > 0)
+		{
+			if(fileread == -1) Err("read");
+			if(write(outft, buf, fileread) == -1) Err("write");
+		}
+		if(close(inft) == -1) Err("close");
+	}
+	if(close(outft) == -1) Err("close");
 	return;
 }
--- projekt/copy.h
+++ projekt/copy.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
+void Err(char* errc);
 void CpFile(char* src, char* dest);
 
 #endif
