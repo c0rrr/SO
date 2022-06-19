2684fe59309bc079434bdc5faba5cabdbddd1617
c0rrr <eryk.surel@onet.pl>
2022-06-19 21:39:42 +0200

    1

--- variables/main.c
+++ variables/main.c
@@ -8,84 +8,80 @@ void *banker(void *ptr);
 void *watcher();
 
 int account = 0;
-pthread_mutex_t transfer_mutex    = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t  transfer_var  = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t transfer_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t transfer_var = PTHREAD_COND_INITIALIZER;
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
-
-int  main()
+int main()
 {
 	int iret1, iret2, iret3;
-    	int bCount = 16;
-    	pthread_t threads[bCount], watcherT;
-    	int tret[16];
+	int bCount = 16;
+	pthread_t threads[bCount], watcherT;
+	int tret[16];
 
-    	long i;
-	int wret = pthread_create( &watcherT, NULL, (void*)watcher, NULL);
-	for(i=0; i<bCount; i++)
+	long i;
+	int wret = pthread_create(&watcherT, NULL, (void *)watcher, NULL);
+	for (i = 0; i < bCount; i++)
 	{
-        	tret[i] = pthread_create( &threads[i], NULL, banker,(void*)i);
-        	if (tret[i])
+		tret[i] = pthread_create(&threads[i], NULL, banker, (void *)i);
+		if (tret[i])
 		{
-          		fprintf(stderr, "ERROR; return code from banker  pthread_create() is %d\n", tret[i]);
-          		exit(EXIT_FAILURE);
-       		}
-    	}
-    	if (wret)
+			fprintf(stderr, "ERROR; return code from banker  pthread_create() is %d\n", tret[i]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (wret)
 	{
-      		fprintf(stderr, "ERROR; return code from watcher pthread_create() is %d\n", wret);
-      		exit(EXIT_FAILURE);
-    	}
+		fprintf(stderr, "ERROR; return code from watcher pthread_create() is %d\n", wret);
+		exit(EXIT_FAILURE);
+	}
 
-    	for(i=0; i<bCount; i++)
+	for (i = 0; i < bCount; i++)
 	{
-        	pthread_join( threads[i], NULL);
-    	}
+		pthread_join(threads[i], NULL);
+	}
 
-    	pthread_join( watcherT, NULL);
-    	exit(EXIT_SUCCESS);
+	pthread_join(watcherT, NULL);
+	exit(EXIT_SUCCESS);
 }
 
-void *banker(void* ptr)
+void *banker(void *ptr)
 {
-	long nr = (long) ptr;
+	long nr = (long)ptr;
 
-    	while(1)
+	while (1)
 	{
-        	pthread_mutex_lock( &transfer_mutex );
-        	int value = 100;
-        	account += value;
-        	//printf("banker nr %ld added %d, account: %d\n", nr, value, account);
+		pthread_mutex_lock(&transfer_mutex);
+		int value = 100;
+		account += value;
 
-        	if (account % 1000 == 0)
+		if (account % 1000 == 0)
 		{
-            		pthread_cond_signal(&transfer_var);
-              		printf("  Watcher signaled, account balance = %d\n", account);
+			pthread_cond_signal(&transfer_var);
+			printf("  Watcher signaled, account balance = %d\n", account);
 		}
 
-        	pthread_mutex_unlock( &transfer_mutex );
-        	usleep(500*1000);
-    	}
-
+		pthread_mutex_unlock(&transfer_mutex);
+		usleep(1000 * 1000);
+	}
 }
 
 void *watcher()
 {
-	int lvar =0;
+	int lvar = 0;
 	printf("Account watcher start\n");
-    	while(1)
+	while (1)
 	{
-        	pthread_mutex_lock(&transfer_mutex);
+		pthread_mutex_lock(&transfer_mutex);
 
-        	while (account % 1000 != 0)
-            		pthread_cond_wait(&transfer_var, &transfer_mutex);
-		if(account != lvar)
+		while (account % 1000 != 0)
+			pthread_cond_wait(&transfer_var, &transfer_mutex);
+		if (account != lvar)
 		{
-			lvar=account;
-        		printf("\tAccount watcher: %d $$\n", account);
+			lvar = account;
+			printf("\tAccount watcher: %d $$\n", account);
 		}
 		pthread_mutex_unlock(&transfer_mutex);
-
-      }
+	}
 }
